0654371273920394e24ccfe50b91d4bd58fa6ed2
leonard-psu <leonard-psu@users.noreply.github.com>
2016-08-01 10:00:49 -0400

    Changes to use Grass 7

--- cf/include/grassio.h
+++ cf/include/grassio.h
@@ -1,7 +1,9 @@
 #ifndef GRASSIO_H
 #define GRASSIO_H
 
+//ORG #include <grass/gis.h>
 #include <grass/gis.h>
+#include <grass/raster.h>
 
 void* raster2array(const char* name, struct Cell_head* header, int* rows,
 		int* cols, RASTER_MAP_TYPE out_type);
